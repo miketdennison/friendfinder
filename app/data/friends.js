d6bdb9b05c07e9849bcb163096ff98fe392176e7
Michael D <miketdennison@gmail.com>
2019-07-16 08:09:12 -0500

    working mvp

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,19 +2,39 @@
 
 // DATABASE
 var friends = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    "name": "Mario",
+    "photo": "https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/bb/8b/eb/bb8beb7b-a7c6-ce90-ef4a-b81cd10d1524/AppIcon-0-1x_U007emarketing-0-85-220-6.png/246x0w.jpg",
+    "scores": [5, 2, 1, 5, 1, 1, 1, 1, 1, 1]
 },
 {
-    "name": "Oner",
-    "photo": "https://www.bhphotovideo.com/images/images2500x2500/phase_one_50500546_capture_one_pro_12_1447832.jpg",
-    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    "name": "Princess Peach",
+    "photo": "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/princess-peach-new-super-mario-bros-wii-35.5.jpg",
+    "scores": [5, 1, 5, 3, 1, 1, 1, 1, 1, 1]
 },
 {
-    "name": "Fiver",
-    "photo": "https://moot.typepad.com/.a/6a00d8341c01ff53ef01b7c73e25c5970b-800wi",
-    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    "name": "Yoshi",
+    "photo": "https://pbs.twimg.com/profile_images/449130100064145410/PNOqhoWA.png",
+    "scores": [5, 2, 1, 2, 1, 5, 1, 5, 1, 5]
+},
+{
+    "name": "Bowser",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/BowserNSMBUD.png/220px-BowserNSMBUD.png",
+    "scores": [1, 5, 1, 1, 1, 5, 5, 3, 5, 3]
+},
+{
+    "name": "Goomba",
+    "photo": "http://www.how-to-draw-cartoons-online.com/image-files/goomba.gif",
+    "scores": [2, 4, 1, 1, 5, 1, 2, 1, 3, 3]
+},
+{
+    "name": "Luigi",
+    "photo": "https://i.redd.it/rs7ew3qulzny.png",
+    "scores": [5, 2, 1, 4, 1, 5, 1, 1, 1, 1]
+},
+{
+    "name": "Wario",
+    "photo": "http://mariopartylegacy.com/wp-content/uploads/2011/08/warioprofile.png",
+    "scores": [1, 5, 1, 2, 1, 1, 1, 1, 5, 5]
 }
 ];
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,7 +4,7 @@ const path = require("path");
 // ROUTES
 module.exports = function(app) {
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"))
+        res.sendFile(path.join(__dirname, "/../public/survey.html")); 
     });
 
     // Default Routing -- must occur after all other routing
